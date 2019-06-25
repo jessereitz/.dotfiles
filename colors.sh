e9d0cb6af5fa48cb733212e278be30bcf5400ddf
Jesse Reitz <jesse@ordoro.com>
2019-06-25 10:03:10 -0500

    Move colors outside of function. Add support for python virtualenvs

--- colors.sh
+++ colors.sh
@@ -1,22 +1,32 @@
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 
+Red='\[\e[0;31m\]'; BRed='\[\e[1;91m\]'
+Gre='\[\e[0;32m\]'; BGre='\[\e[1;92m\]'
+Yel='\[\e[0;33m\]'; BYel='\[\e[1;93m\]'
+Blu='\[\e[0;34m\]'; BBlu='\[\e[1;94m\]'
+Mag='\[\e[0;35m\]'; BMag='\[\e[1;95m\]'
+Cya='\[\e[0;36m\]'; BCya='\[\e[1;96m\]'
+Whi='\[\e[0;37m\]'; BWhi='\[\e[1;97m\]'
+None='\[\e[0m\]'
+
+function set_virtualenv () {
+  if test -z "$VIRTUAL_ENV" ; then
+      PYTHON_VIRTUALENV=""
+  else
+      PYTHON_VIRTUALENV="$None[$Mag`basename \"$VIRTUAL_ENV\"`$None] "
+  fi
+}
+
 bash_prompt() {
-    local Red='\[\e[0;31m\]'; local BRed='\[\e[1;31m\]'
-    local Gre='\[\e[0;32m\]'; local BGre='\[\e[1;32m\]'
-    local Yel='\[\e[0;33m\]'; local BYel='\[\e[1;33m\]'
-    local Blu='\[\e[0;34m\]'; local BBlu='\[\e[1;34m\]'
-    local Mag='\[\e[0;35m\]'; local BMag='\[\e[1;35m\]'
-    local Cya='\[\e[0;36m\]'; local BCya='\[\e[1;36m\]'
-    local Whi='\[\e[0;37m\]'; local BWhi='\[\e[1;37m\]'
-    local None='\[\e[0m\]'
+    set_virtualenv
 
     local GIT_PS1_SHOWDIRTYSTATE=True
     local GIT_PS1_SHOWSTASHSTATE=True
     local GIT_PS1_SHOWCOLORHINTS=True
     local GIT_PS1_SHOWUNTRACKEDFILES=True
 
-    __git_ps1 "$Cya\u$None@$Gre\h:$Yel\w$None" "$None$ "
+    __git_ps1 "$PYTHON_VIRTUALENV$Cya\u$None@$Gre\h:$Yel\w$None" "$None$ "
 }
 
 
