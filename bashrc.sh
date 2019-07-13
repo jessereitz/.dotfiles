0c0a0bcf4408f29ba9daca7646cf75636e35ddad
Jesse Reitz <jessereitz1@gmail.com>
2019-07-12 21:19:00 -0500

    Load bonnie only if it exists

--- bashrc.sh
+++ bashrc.sh
@@ -2,7 +2,10 @@ source ~/.bash_settings/variables.sh
 source ~/.bash_settings/functions.sh
 source ~/.bash_settings/aliases.sh
 source ~/.bash_settings/colors.sh
-source ~/bonnie/bonnie.sh
+
+if [ -f ~/bonnie/bonnie.sh ]; then
+  source ~/bonnie/bonnie.sh
+fi
 
 if [ -f ~/.bash_settings/git-completion.bash ]; then
   . ~/.bash_settings/git-completion.bash
