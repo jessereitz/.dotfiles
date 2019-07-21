73e417d554318c89f398b3a358632847049ee6af
Jesse Reitz <jessereitz1@gmail.com>
2019-07-21 15:55:31 -0500

    Add terminal backup/restore function.

--- functions.sh
+++ functions.sh
@@ -6,6 +6,51 @@ function open() {
     open $* &
   fi
 }
+
+
+# Backup/Restore Gnome terminal
+function terminal() {
+  if [ ! $OS_ENV == "Linux" ]; then
+    echo "This isn't Linux. Don't even try."
+    exit
+  fi
+
+  settings_file=~/.gnome_terminal_settings.txt
+  action=$1
+
+  case "$action" in
+    backup)     ;;
+    load)       ;;
+    *)          action="ERR";;
+  esac
+
+  if [ $action = "ERR" ]; then
+    echo "Invalid action ${action}."
+    echo "Usage: terminal [action=backup|load] <settings_file>"
+  fi
+
+  if [ $action = "backup" ]; then
+    echo "Saving Gnome terminal settings to ${settings_file}"
+    dconf dump /org/gnome/terminal/ > $settings_file
+    echo "Done."
+  fi
+
+  if [ $action = "load" ]; then
+    custom_settings_file=$2
+    if [ -z $custom_settings_file ]; then
+      echo "No settings file provided. Using default $settings_file"
+    else
+      settings_file=$custom_settings_file
+    fi
+
+    echo "Resetting settings."
+    dconf reset -f /org/gnome/terminal/
+
+    echo "Loading custom settings."
+    dconf load /org/gnome/terminal/ < $settings_file
+  fi
+}
+
 function oprah() {
   GIT_ROOT=$(git rev-parse --show-toplevel)
   docker run \
