374cbe08191c8ada89ea6c8f2d27d08be8738703
Jesse Reitz <jessereitz1@gmail.com>
2019-07-21 14:59:04 -0500

    Comment out ordoro stuff

--- bashrc.sh
+++ bashrc.sh
@@ -2,7 +2,7 @@ source ~/.bash_settings/variables.sh
 source ~/.bash_settings/functions.sh
 source ~/.bash_settings/aliases.sh
 source ~/.bash_settings/colors.sh
-source ~/bonnie/bonnie.sh
+#source ~/bonnie/bonnie.sh
 
 if [ -f ~/.bash_settings/git-completion.bash ]; then
   . ~/.bash_settings/git-completion.bash
