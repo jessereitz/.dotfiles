e7f1044728ee15619939d1eadb2e98924c0e81d1
Jesse Reitz <jesse@ordoro.com>
2019-10-08 17:44:51 -0500

    Add some aliases
    * py3 -> cuz I'm lazy
    * gti -> cuz I can't type

--- aliases.sh
+++ aliases.sh
@@ -7,7 +7,9 @@ alias ccat="source-highlight --out-format=esc256 -o STDOUT -i"
 alias exe="chmod +x $1"
 alias getip="curl icanhazip.com"
 alias py="python"
+alias py3="python3"
 alias cod="code"
+alias gti="git"
 
 if [ $OS_ENV == "Linux" ]; then
   alias open="xdg-open"
