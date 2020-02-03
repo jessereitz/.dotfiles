558819c31ded6cc11f343f4f6ed7057fc94a584f
Jesse Reitz <jesse@ordoro.com>
2020-02-03 13:57:10 -0600

    Allow settable display_hostname
    * I tried this before but this way should actually work

--- main_config.sh
+++ main_config.sh
@@ -166,13 +166,15 @@ function set_virtualenv () {
 
 bash_prompt() {
     set_virtualenv
-
+    if [ -z $DISPLAY_HOSTNAME ]; then
+        DISPLAY_HOSTNAME=`hostname`
+    fi
     local GIT_PS1_SHOWDIRTYSTATE=True
     local GIT_PS1_SHOWSTASHSTATE=True
     local GIT_PS1_SHOWCOLORHINTS=True
     local GIT_PS1_SHOWUNTRACKEDFILES=True
 
-    __git_ps1 "$PYTHON_VIRTUALENV$Cya\u$None@$Gre\h:$Yel\w$None" "$None$ "
+    __git_ps1 "$PYTHON_VIRTUALENV$Cya\u$None@$Gre$DISPLAY_HOSTNAME:$Yel\w$None" "$None$ "
 }
 
 PROMPT_COMMAND=bash_prompt
