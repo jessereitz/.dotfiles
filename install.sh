762538993e21b4efba5190e3bcd5dc7f3feaeeab
Jesse Reitz <jessereitz1@gmail.com>
2020-04-23 17:14:12 -0500

    Some more install updates
    * Make sure all prereqs installed
    * Include some more output
    * Simplify function calls

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ echo "This script will install ReReitz dotfiles into $HOME/.dotfiles"
 echo
 
 COMPLETE_INSTALL=false
+OS_ENV="$(uname -s)"
 
 echo "Please select an installation type:"
 echo -e "\t 1) Dotfiles only (clone via https, set up bash and vim)"
@@ -25,11 +26,11 @@ fi
 echo $COMPLETE_INSTALL
 
 function install_dotfiles {
+    echo "Installing dotfiles..."
     dotfiles_dir=$HOME/.dotfiles
     dotfiles_repo=https://github.com/jessereitz/.dotfiles
 
-    unameOut="$(uname -s)" &&
-    case "${unameOut}" in
+    case "${OS_ENV}" in
         Linux*)     config_file=~/.bashrc;;
         Darwin*)    config_file=~/.bash_profile;;
         *)          echo "unkown OS" && exit;;
@@ -51,32 +52,35 @@ function install_dotfiles {
     initialize_all
 }
 
-function install_everything {
-    OS_ENV="$(uname -s)" &&
-    if [ $OS_ENV == "Linux" ]; then
-        sudo apt-get install build-essential -y
-        homebrew_prefix=/home/linuxbrew/.linuxbrew
-    elif [ $OS_ENV == "Darwin" ]; then
-        homebrew_prefix=/usr/local
-    else
-        echo "unkown OS" && exit
+function install_brew_and_packages {
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    if [ $OS_ENV == 'Linux' ]; then
+        test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+        test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+        test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
+        echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
     fi
-
-    homebrew_exec=$homebrew_prefix/bin/brew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
+    echo "Installing apps..."
     curl -o /tmp/Brewfile https://gist.githubusercontent.com/jessereitz/a6f0c8ccc0741b224a64c14368a9b92c/raw/ &&
-    test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
-    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
+    brew bundle --file /tmp/Brewfile
+    echo "Homebrew and apps installed."
+}
 
-    brew bundle --file /tmp/Brewfile &&
-    install_dotfiles -h
+function install_prereqs {
+    echo "Installing prerequisite packages..."
+    if [ $OS_ENV == "Linux" ]; then
+        sudo apt-get install git curl build-essential -y
+    elif [ $OS_ENV == "Darwin" ]; then
+        xcode-select --install
+    fi
+    echo "All prerequisite packages installed."
 }
 
 
 if [ $COMPLETE_INSTALL = true ]; then
-    install_everything
+    install_brew_and_packages
+    install_dotfiles -h
 else
     install_dotfiles
 fi
